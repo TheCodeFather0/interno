127405652a79c042bcf3714390e5ac6075a914cc
TheCodeFather0 <raminmammadov056@gmail.com>
2023-10-25 12:27:59 +0400

    navigation metnu created

---
+++ src/Pages/HomePage/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navigation from "../../Components/Navigation";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Navigation />
+    </div>
+  );
+};
+
+export default HomePage;
