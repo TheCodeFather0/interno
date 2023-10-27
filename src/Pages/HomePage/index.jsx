79a5fdf8bc27d030d63a2c7fcfc18a1227cb62ec
TheCodeFather0 <raminmammadov056@gmail.com>
2023-10-27 11:30:16 +0400

    banner added homepage

--- src/Pages/HomePage/index.jsx
+++ src/Pages/HomePage/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navigation from "../../Components/Navigation";
+import Banner from "../../Components/Banner";
 
 const HomePage = () => {
   return (
     <div>
       <Navigation />
+      <Banner />
     </div>
   );
 };
