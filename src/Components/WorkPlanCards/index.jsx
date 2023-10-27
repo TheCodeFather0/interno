93bc4692d3d67085bac711da873f0b6ec007d059
TheCodeFather0 <raminmammadov056@gmail.com>
2023-10-27 12:03:45 +0400

    workplancard created

---
+++ src/Components/WorkPlanCards/index.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import styles from "./style.module.css";
+
+const _url = "http://localhost:3000/WorkPlan";
+const WorkPlanCards = () => {
+  const [cards, setCards] = useState([]);
+  useEffect(() => {
+    axios.get(_url).then(({ data }) => {
+      setCards(data);
+    });
+  }, []);
+  return (
+    <div className="container">
+      <div className="row mb-5 mt-2">
+        {cards.map(({ id, title, description }) => {
+          return (
+            <div
+              className={`col-12 col-xl-4 my-3 ${styles.WorkPlanCard}`}
+              key={id}
+            >
+              <h3 className={`my-3 ${styles.title}`}>{title}</h3>
+              <p>{description}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default WorkPlanCards;
+
--- src/Pages/HomePage/index.jsx
+++ src/Pages/HomePage/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navigation from "../../Components/Navigation";
 import Banner from "../../Components/Banner";
+import WorkPlanCards from "../../Components/WorkPlanCards";
 
 const HomePage = () => {
   return (
     <div>
       <Navigation />
       <Banner />
+      <WorkPlanCards />
     </div>
   );
 };
